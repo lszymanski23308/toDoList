f3ad293a166c132c7cc779e7a47af21122dcd76a
lszymanski23308 <lszymanski23308@gmail.com>
2023-03-22 18:59:16 +0100

    add and remove new tasks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 {
   const tasks = [
     {
-      content: "test",
+      content: "wyjsc z psem",
       done: false,
     },
     {
-      content: "test",
+      content: "zrobic obiad",
       done: true,
     }
   ];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  }
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  }
 
   const render = () => {
     let htmlString = "";
@@ -18,22 +31,28 @@
       <li
       ${task.done ? " style = \"text-decoration: line-through\"" : ""}
       >
+      <button class="js-remove">usuń</button>
       ${task.content}
       </li>
       `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
+
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index)
+      });
+
+    });
   };
 
 
-  const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
 
-    render();
-  }
+
 
   const onFormSubmit = (event) => {
     event.preventDefault();
